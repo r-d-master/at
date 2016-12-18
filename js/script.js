f5d434078e4908a09fc774ebeb2f431198a53984
R D Master <gsjha@tcss.co.in>
2016-12-18 16:09:02 +0530

    frFrom-secondSet

--- js/script.js
+++ js/script.js
@@ -44,6 +44,49 @@ $(".previouss").click(function(){
 	
 });
 
+$(".nextBtnp").click(function(){
+	
+	
+	current_fs = $(this).parent().parent().parent();
+	next_fs = $(this).parent().parent().parent().next();	
+
+	//show the next fieldset
+	next_fs.show();
+
+	next_fs.show(); 
+	current_fs.hide();
+	
+});
+
+$(".previousp").click(function(){
+	
+	current_fs = $(this).parent().parent().parent();
+	previous_fs = $(this).parent().parent().parent().prev();
+	
+	//de-activate current step on progressbar
+	//$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	current_fs.hide();
+	//hide the current fieldset with style
+	
+});
+$(".previoussp").click(function(){
+	
+	current_fs = $(this).parent().parent().parent().parent().parent();
+	previous_fs = $(this).parent().parent().parent().parent().parent().prev();
+	
+	//de-activate current step on progressbar
+	//$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	current_fs.hide();
+	//hide the current fieldset with style
+	
+});
+
 $(".submit").click(function(){
 	return false;
 })
