be54d641458815fdd0be36412a5d32a67c1e4532
R D Master <gsjha@tcss.co.in>
2016-12-21 12:29:39 +0530

    frFrom-fixTable

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 
 	$(document).ready(function(){
 
-		
+		$(".owl-carousel").owlCarousel({
+		    loop:true,
+		    margin:10,
+		    responsiveClass:true,
+		    responsive:{
+		        0:{
+		            items:1,
+		            /*nav:true*/
+		        },
+		        600:{
+		            items:1,
+		            nav:false
+		        },
+		        1000:{
+		            items:1,
+		            nav:false,
+		            /*loop:false*/
+		        }
+		    }
+		});
 
 		$(".nextBtn").click(function(){
 	
